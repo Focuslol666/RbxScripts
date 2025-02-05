f4de1ea562741dfa7b33cc54356f63e63417f19e
Focuslol666 <focus_114514@qq.com>
2025-02-05 23:33:14 +0800

    Create DropKick.lua

---
+++ Other/DropKick.lua
@@ -0,0 +1,8 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = "FUCK YOU",
+		Text = "Loading DropKick... Please wait, Shit Bro",
+		Duration = 3
+	})
+wait(5)
+
+game.Players.LocalPlayer:Kick("20-20-20  GET OUT!")
