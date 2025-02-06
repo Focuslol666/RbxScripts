1fc580d87d78bd4dc50c16621751a76f84f56d6e
Focuslol666 <focus_114514@qq.com>
2025-02-06 22:32:42 +0800

    Update DropKick.lua

--- Other/DropKick.lua
+++ Other/DropKick.lua
@@ -6,7 +6,7 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 wait(4.5)
 game.Players.LocalPlayer.Character.Humanoid.Health = 0
 print("Fuck You")
-warn("Holy shit bro")
+warn("Holy shit Motherfucker")
 error("You son of a bitch")
 wait(0.01)
 local spawn_nm = Instance.new("Sound", workspace)
