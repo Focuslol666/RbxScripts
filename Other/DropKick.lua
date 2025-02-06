d811f1ab113aa1967459aabb8756ec91be9566c2
Focuslol666 <focus_114514@qq.com>
2025-02-06 20:06:20 +0800

    Update DropKick.lua

--- Other/DropKick.lua
+++ Other/DropKick.lua
@@ -3,7 +3,9 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 		Text = "Loading DropKick... Please wait, Shit Bro",
 		Duration = 3
 	})
-wait(4)
+wait(4.5)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(0.01)
 local spawn_nm = Instance.new("Sound", workspace)
  
 spawn_nm.SoundId = "rbxassetid://8350106890"
