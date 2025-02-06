37f677f9da0261e8ec8ace47ade0dcf66f13b90b
Focuslol666 <focus_114514@qq.com>
2025-02-06 15:28:11 +0800

    Update DropKick.lua

--- Other/DropKick.lua
+++ Other/DropKick.lua
@@ -6,11 +6,10 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 wait(5)
 local spawn_nm = Instance.new("Sound", workspace)
  
-spawn_nm.SoundId = "https://github.com/Focuslol666/Scripts/blob/cd391e295e8b8d9eb1ef749b0b01155a01e18823/Other/A60JumpscareOuch.mp3?raw=true"
+spawn_nm.SoundId = "rbxassetid://8350106890"
  
 spawn_nm.Volume = 10
  
 spawn_nm:Play()
+wait(0.5)
 game.Players.LocalPlayer:Kick("20-20-20  GET OUT!")
-
-end
