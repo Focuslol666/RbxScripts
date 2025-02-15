d28d6cecbc68e0c063a13d689d0033e8a4f9ed0f
Focuslol666 <focus_114514@qq.com>
2025-02-15 15:22:19 +0800

    Update Fake RTX.lua

--- Other/Fake RTX.lua
+++ Other/Fake RTX.lua
@@ -1,8 +1,10 @@
+-- The raw file is not mine. I just uploaded it to Github. Raw file is below.
+
 -- RTX OMG
 
 -- script model: https://bit.ly/3uAMBCg
 
--- raw: https://bit.ly/3B6KOYa
+-- raw: https://bit.ly/3B6KOYa (Original Creator's Raw)
 
 local Vignette = true -- change to false if you don't want a shadow frame
 
