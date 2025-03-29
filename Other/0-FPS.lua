2213d4978d0e1eca6b668ee4b253e475a22df17a
Focuslol666 <focus_114514@qq.com>
2025-03-29 22:44:26 +0800

    Update 0-FPS.lua

--- Other/0-FPS.lua
+++ Other/0-FPS.lua
@@ -1,7 +1,7 @@
 while true do
-  print("Your Client FPS: 0 !")
-  warn("Your Client FPS: 0 !!")
-  error("Your Client FPS: 0 !!!")
+print("Your Client FPS: 0 !")
+warn("Your Client FPS: 0 !!")
+error("Your Client FPS: 0 !!!")
 end
 
 -- Don't ask me why I use three print types, because I think it's cool... (and it doesn't really work) 🧐🧐🧐
