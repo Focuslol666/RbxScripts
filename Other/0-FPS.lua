e734b37c17cea1c61d161c86181758ae74c8f8c1
Focuslol666 <focus_114514@qq.com>
2025-03-29 22:37:13 +0800

    Update 0-FPS.lua

--- Other/0-FPS.lua
+++ Other/0-FPS.lua
@@ -1,6 +1,3 @@
--- This is a Troll Script that will make your phone stutter to the point of being unusable and can be used to troll your friends! 🤓☝️
--- On April Fool's Day, troll your friend who doesn't know how to use Github, because he won't come to check out my library. 😎
-
 while true do
   print("Your Client FPS: 0 !")
   warn("Your Client FPS: 0 !!")
