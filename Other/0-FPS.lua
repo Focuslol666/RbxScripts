1b8cd55ce0410d3c992813702cf43843004a0605
Focuslol666 <focus_114514@qq.com>
2025-03-29 19:01:03 +0800

    Update 0-FPS.lua

--- Other/0-FPS.lua
+++ Other/0-FPS.lua
@@ -1,5 +1,5 @@
 while true do
-  print("Your Client FPS: 0")
-  warn("Your Client FPS: 0")
-  error("Your Client FPS: 0")
+  print("Your Client FPS: 0 !")
+  warn("Your Client FPS: 0 !!")
+  error("Your Client FPS: 0 !!!")
 end
