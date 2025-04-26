f64a28be506dec398400e2e6fb0c4f26dd76efb7
FOCUS <focus_114514@qq.com>
2025-04-26 22:20:39 +0800

    Delete Other/Fools.lua

--- Other/Fools.lua
+++
@@ -1,7 +0,0 @@
--- WARNING!
--- This script may cause your account to be Banned! Please use with caution! (Reason: Sensitive Words)
-
-while true do
-    game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("FUCK YOU!!!")
-    wait(0)
-end
