32ed8307ff3dda4dbd6da5611328ec38f3f47f4f
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-06-09 22:20:01 +0800

    Update A-120.lua

--- DOORS/RoomsEntities/A-120.lua
+++ DOORS/RoomsEntities/A-120.lua
@@ -6,7 +6,7 @@ local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regul
 
 local entity = spawner.Create({
 	Entity = {
-		Name = "A120",
+		Name = "A-120",
 		Asset = "rbxassetid://12907330705",
 		HeightOffset = 0
 	},
