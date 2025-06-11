a534d2de5e56c00afb8aec104d9c14b694bb602b
FOCUS <focus_114514@qq.com>
2025-06-11 19:27:57 +0800

    Create A-90_BranchToASH500.lua

---
+++ DOORS/RoomsEntities/A-90/A-90_BranchToASH500.lua
@@ -0,0 +1,11 @@
+_G.A90Config = {
+    Face = "rbxassetid://11924478218",
+    AngryFace = "rbxassetid://11924266902",
+    StopIcon = "rbxassetid://12246141362",
+    StaticColor = Color3.fromRGB(255, 0, 0),
+    HitSound = "rbxassetid://11926070708",
+    SpawnSound = "rbxassetid://11924500496",
+    Damage = 23500
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/DOORS/RoomsEntities/A-90/A-90_Spawn.lua"))()
