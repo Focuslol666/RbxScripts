d32d1a3aaa5dd1f4ae65f625b7bec15fca1b81d3
FOCUS <focus_114514@qq.com>
2025-06-14 23:01:18 +0800

    Add files via upload

---
+++ DOORS/RoomsEntities/A-90/A-90_BranchToASH500.lua
@@ -0,0 +1,14 @@
+_G.A90Config = {
+    Face = "rbxassetid://11924478218",
+    AngryFace = "rbxassetid://11924266902",
+    StopIcon = "rbxassetid://12246141362",
+    StaticColor = Color3.fromRGB(255, 0, 0),
+    HitSound = "rbxassetid://11926070708",
+    SpawnSound = "rbxassetid://11924500496",
+    Damage = 23500,
+    Cause = "A-90",
+    Hints = {"You died to A-90", "Stop your Steps!"},
+    HintType = "Yellow"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/DOORS/RoomsEntities/A-90/A-90_Source.lua"))()
