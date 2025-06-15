d0d1a369805cdda29d0bc5095197078226bad5de
FOCUS <focus_114514@qq.com>
2025-06-15 23:57:04 +0800

    Update A-90_Example.lua

--- DOORS/RoomsEntities/A-90/A-90_Example.lua
+++ DOORS/RoomsEntities/A-90/A-90_Example.lua
@@ -2,18 +2,24 @@
 -- Made by @Focuslol666
 -- Version: v1_Alpha
 
--- Default Config
+---=== Set the Config ===---
+-- Default
 _G.A90Config = {
-    Face = "rbxassetid://11924478218",
-    AngryFace = "rbxassetid://11924266902",
-    StopIcon = "rbxassetid://12246141362",
-    StaticColor = Color3.fromRGB(255, 0, 0),
-    HitSound = "rbxassetid://11926070708",
-    SpawnSound = "rbxassetid://11924500496",
-    Damage = 90,
-    Cause = "A-90",
-    Hints = {"You died to A-90", "Stop your Steps!"},
-    HintType = "Yellow" -- "Blue" = Guiding, "Yellow" = Curious
+    Assets = {
+        Face = "rbxassetid://11924478218",
+        AngryFace = "rbxassetid://11924266902",
+        StopIcon = "rbxassetid://12246141362",
+        StaticColor = Color3.fromRGB(255, 0, 0),
+        HitSound = "rbxassetid://11926070708",
+        SpawnSound = "rbxassetid://11924500496"
+    },
+    Death = {
+        Damage = 90,
+        Cause = "A-90",
+        Hints = {"You died to A-90", "Stop your Steps!"},
+        HintType = "Yellow" -- "Blue" = Guiding, "Yellow" = Curious
+    }
 }
 
+---=== Apply the Config ===---
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/DOORS/RoomsEntities/A-90/A-90_Source.lua"))()
