850cf45bfe1bf39eca6b64a36d1fa722372922c1
FOCUS <focus_114514@qq.com>
2025-11-23 13:44:23 +0800

    Add files via upload

---
+++ DOORS/Custom Trial And Error/Example.lua
@@ -0,0 +1,12 @@
+local GlitchTrial = loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/DOORS/Custom%20Trial%20And%20Error/Source.lua"))()
+
+GlitchTrial:SetConfig({
+    Glitch = true,
+    Text = "NDI5OiBUT08gTUFOWSBSRVFVRVNUUw==",
+    Ending = {
+        Enabled = true,
+        Delay = 18
+    }
+})
+
+GlitchTrial:Activate()
