fe7a4bc205152989c50f96a193dee5663ada2069
FOCUS <focus_114514@qq.com>
2025-11-23 13:25:22 +0800

    Delete DOORS/Custom Trial And Error directory

--- DOORS/Custom Trial And Error/Example.lua
+++
@@ -1,8 +0,0 @@
-local GlitchTrial = loadstring(game:HttpGet(""))()
-
-GlitchTrial:SetConfig({
-    Glitch = true,
-    Text = "NDI5OiBUT08gTUFOWSBSRVFVRVNUUw==",
-    Delay = 18
-})
-GlitchTrial:Activate()
